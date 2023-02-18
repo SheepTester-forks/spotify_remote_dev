6cd32cbd779619aed2cfbca1fdf4826d72881120
Sean <ussa.say@gmail.com>
2023-02-17 19:57:37 -0800

    wow it uploads

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
-#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include "secrets.h"
+
+WiFiClientSecure client;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print("fuck");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(WIFI_SSID);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Do nothing
 }
